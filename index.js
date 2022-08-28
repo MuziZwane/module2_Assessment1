bd060bbe2fea5a3aae330dcd2cc6b455ff2e2545
Muzi Zwane <87149575+MuziZwane@users.noreply.github.com>
2022-08-28 16:30:52 +0200

    Create index.js
    a) Log your information (names, institution, and Github username) into the console log with a statement that introduces you, e.g. “My name is Tsakane, a student from Xawe University of Technology and my Github username is

---
+++ index.js
@@ -0,0 +1,8 @@
+// my variables
+
+var names = "Muzi Zwane";
+var institution = "UJ";
+var github = "MuziZwane";
+
+// display results
+console.log("my name is " + names + " " + "a student from " + institution + ". my Github username is " + github);
